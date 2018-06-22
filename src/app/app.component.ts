a29481220174e754cc1faf90cad4b33971dcf010
Oleg <oleh.mykhalyna@otakoyi.com>
2018-06-22 17:51:35 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    products: Product[];
+      constructor() {
+          this.products = [
+              new Product(
+              'NICEH1',
+              'A Nice Black Hat',
+              '/assets/images/products/black-hat.jpg',
+              ['Men', 'Accessories', 'Hats'],
+              29.212),
+              new Product(
+                  'NEATOJACKET',
+                  'A Nice green Hat',
+              '/assets/images/products/black-hat.jpg',
+                  ['Women', 'Apparel', 'Jackets & Vests'],
+                  56.99),
+              new Product(
+                  'NICEHAT123',
+                  'A Nice red Hat',
+                  '/assets/images/products/black-hat.jpg',
+                  ['Men', 'Accessories', 'Hats'],
+                  19.99)
+          ];
+      }
+      productWasSelected(product: Product) {
+          console.log('Product was selected ', product);
+      }
 }
